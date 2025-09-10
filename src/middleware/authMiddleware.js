b9f0ce4df70f8b833c331febf9398e0a562d13bb
ayush gupta <ag298744@gmail.com>
2025-09-10 21:55:31 +0530

    new

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,26 +1,19 @@
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
-
 
 export const protect = (req, res, next) => {
-    const token = req.cookies.token; // ✅ from cookies
+  const token = req.cookies.token;
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
 
-    if (!token) return res.status(401).json({ message: "Unauthorized" });
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (err) {
-        res.status(401).json({ message: "Invalid token" });
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
 };
 
 export const adminOnly = (req, res, next) => {
-  if (!req.user?.isAdmin) {
-    return res.status(403).json({ message: "Forbidden: Admins only" });
-  }
+  if (!req.user?.isAdmin) return res.status(403).json({ message: "Admins only" });
   next();
 };
-
