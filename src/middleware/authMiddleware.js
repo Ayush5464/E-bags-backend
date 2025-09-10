576ca28dc9323e89499b504e68efd734143a20c6
ayush gupta <ag298744@gmail.com>
2025-09-10 21:39:20 +0530

    new

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,23 +1,26 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import User from "../models/userModel.js"; // import your user model
-
 dotenv.config();
 
-export const protect = async (req, res, next) => {
-  const token = req.cookies.token;
 
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
+export const protect = (req, res, next) => {
+    const token = req.cookies.token; // ✅ from cookies
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
 
-    const user = await User.findById(decoded.userId).select("-password");
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    req.user = user; //  full user attached
-    next();
-  } catch (err) {
-    res.status(401).json({ message: "Invalid token" });
-  }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: "Invalid token" });
+    }
 };
+
+export const adminOnly = (req, res, next) => {
+  if (!req.user?.isAdmin) {
+    return res.status(403).json({ message: "Forbidden: Admins only" });
+  }
+  next();
+};
+
